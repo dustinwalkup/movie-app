5d8ee062974495d45129329c2509c049e548e2f0
Dustin Walkup <dustin.walkup@levelfivesolutions.com>
2025-04-08 21:13:29 -0500

    fix build error

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ export { default as AuthModal } from "./auth-modal";
 export { default as BackButton } from "./back-button";
 export { default as Icon } from "./icon";
 export { default as MobileNav } from "./mobile-nav";
-export { default as Navbar } from "./navbar";
+// export { default as Navbar } from "./navbar";
 export { default as SearchInput } from "./search-input";
 export { default as StreamingIcon } from "./streaming-icon";
 export { default as TrailerButton } from "./trailer-button";
